f584b30ea78edde6e47304dc9c259f4ca21dcd5a
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-22 13:12:15 +0200

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Martin Flischman on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
